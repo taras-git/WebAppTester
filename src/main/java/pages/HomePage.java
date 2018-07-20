d8bc748d77ae6157547ae64d4491524a2505a88a
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2018-07-20 15:56:29 +0300

    Introduce modified Page Object with initializing pages before executing a test

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,20 +4,27 @@ import baseclasses.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import utils.JsonReader;
 
 /**
  * Created by taras on 7/19/18.
  */
 public class HomePage extends BasePage{
 
+    protected String homePageEn = JsonReader.getPropertyFileValue("home_page_en");
+
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    public BookingPage openBooking() {
+    public HomePage start() {
+        driver.get(homePageEn);
+        return this;
+    }
+
+    public void clickBookVehicle() {
         WebElement bookVehicle=driver.findElement(By.xpath("//nav[@id=\"top-navigation\"]" +
                 "//*[contains(text(), 'Book Vehicle')]"));
         bookVehicle.click();
-        return new BookingPage(this.driver);
     }
 }
