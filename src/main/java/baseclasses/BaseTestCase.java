929d36114436a2ce8c2b89d1ad5388595c412cac
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2018-07-20 16:42:23 +0300

    add new way to create a property value in testng.xml

--- src/main/java/baseclasses/BaseTestCase.java
+++ src/main/java/baseclasses/BaseTestCase.java
@@ -2,6 +2,7 @@ package baseclasses;
 
 import driver.A2Driver;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import pages.BookingPage;
@@ -17,10 +18,18 @@ public class BaseTestCase {
 
     protected HomePage homePage;
     protected BookingPage bookingPage;
+    String browser;
 
     @BeforeTest
-    public void getDriver() throws Exception {
-        driver = new A2Driver(JsonReader.getPropertyFileValue("browser"));
+    public void getDriver(ITestContext context) throws Exception {
+        // get the browser from XML testng file
+        browser = context.getCurrentXmlTest().getParameter("browser");
+
+        // or
+        // get the browser from JSON property file
+        //browser = JsonReader.getPropertyFileValue("browser");
+
+        driver = new A2Driver(browser);
         driver.manage().window().maximize();
         initPages();
     }
