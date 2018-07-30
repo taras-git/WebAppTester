c559d1de647fba612c5198349e83c10cb357294a
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2018-07-30 11:06:37 +0300

    refactored test cases

---
+++ src/main/java/tests/A2DBookingTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import baseclasses.BaseTestCase;
+import org.testng.annotations.Test;
+import utils.JsonReader;
+import utils.Utils;
+
+/**
+ * Created by taras on 7/17/18.
+ */
+public class A2DBookingTest extends BaseTestCase {
+
+    private final String locationName = JsonReader.getPropertyFileValue("location");
+    private final String email = Utils.getTestUserEmail();
+    private final String password = Utils.getTestUserPassword();
+
+    @Test
+    public void bookVehicleUserNotLogged(){
+        homePage.start()
+                .clickBookVehicle();
+
+        bookingPage.verifyBookingPageDisplayed()
+                .fillCheckOut()
+                .fillCheckIn()
+                .chooseLocation(locationName);
+
+        bookingPage.clickFindCar();
+
+        chooseCarPage.waitChooseCarDisplayed()
+                .clickChooseCar();
+
+        loginPage.verifyLoginWarningDisplayed();
+    }
+
+    @Test
+    public void loggedUserCanBookVehicle(){
+
+        homePage.start()
+                .clickBookVehicle();
+
+        bookingPage.verifyBookingPageDisplayed()
+                .fillCheckOut()
+                .fillCheckIn()
+                .chooseLocation(locationName);
+
+        bookingPage.clickFindCar();
+
+        chooseCarPage.waitChooseCarDisplayed()
+                .clickChooseCar();
+
+        loginPage.verifyLoginWarningDisplayed()
+                .login(email, password)
+                .verifyUserLogged();
+    }
+}
--- src/main/java/tests/A2DNavigationTests.java
+++ src/main/java/tests/A2DNavigationTests.java
@@ -2,18 +2,11 @@ package tests;
 
 import baseclasses.BaseTestCase;
 import org.testng.annotations.Test;
-import utils.JsonReader;
-import utils.Utils;
 
 /**
- * Created by taras on 7/17/18.
+ * Created by taras on 7/30/18.
  */
-public class A2DTest extends BaseTestCase {
-
-    private final String locationName = JsonReader.getPropertyFileValue("location");
-    private final String email = Utils.getTestUserEmail();
-    private final String password = Utils.getTestUserPassword();
-
+public class A2DNavigationTests extends BaseTestCase{
     @Test
     public void navigateToBookingPage() {
         homePage.start()
@@ -88,43 +81,4 @@ public class A2DTest extends BaseTestCase {
         contactPage.verifyContactPageDisplayed();
     }
 
-
-    @Test
-    public void bookVehicleUserNotLogged(){
-        homePage.start()
-                .clickBookVehicle();
-
-        bookingPage.verifyBookingPageDisplayed()
-                .fillCheckOut()
-                .fillCheckIn()
-                .chooseLocation(locationName);
-
-        bookingPage.clickFindCar();
-
-        chooseCarPage.waitChooseCarDisplayed()
-                .clickChooseCar();
-
-        loginPage.verifyLoginWarningDisplayed();
-    }
-
-    @Test
-    public void loggedUserCanBookVehicle(){
-
-        homePage.start()
-                .clickBookVehicle();
-
-        bookingPage.verifyBookingPageDisplayed()
-                .fillCheckOut()
-                .fillCheckIn()
-                .chooseLocation(locationName);
-
-        bookingPage.clickFindCar();
-
-        chooseCarPage.waitChooseCarDisplayed()
-                .clickChooseCar();
-
-        loginPage.verifyLoginWarningDisplayed()
-                .login(email, password)
-                .verifyUserLogged();
-    }
 }
